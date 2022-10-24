30346b3b35a03d7f415cffbd3c92f9c20b3be564
thaiduy1704 <thaiduy1704@gmail.com>
2022-10-24 18:49:29 +0700

    Bug Scroll

---
+++ .history/src/components/Home_20221024182347.tsx
@@ -0,0 +1,65 @@
+import React from "react";
+import {motion} from 'framer-motion'
+import { HiArrowNarrowRight } from "react-icons/hi";
+import Avatar from "../assets/homeAva.png";
+  
+const Home = () => {
+  return (
+    <div className="   w-full h-screen flex flex-col lg:flex-row justify-center items-center mr-10 bg-[#0a192f] gap-8">
+      {/* Container */}
+      <motion.div
+
+       layout
+  initial={{ opacity: 0.5,x:-500  }}
+  animate={{ opacity: 1, x:-100 }}
+  transition={{
+   
+    opacity: { ease: "linear" },
+    layout: { duration: 0.5 }
+  }}
+
+      
+      className="content_home  flex justify-center items-center ">
+        <div className=" mt-[5rem] ml-[10rem] max-w-[500px] md:max-w-[1000px] md:ml-[5rem] flex flex-col justify-center h-full">
+          <p className="text-pink-600 text-2xl">Hi, My name is </p>
+          <h1 className="text-4xl md:text-7xl font-bold  text-[#ccd6f6]">
+            Nguyen Tran Thai Duy
+          </h1>
+          <h2 className="text-4xl md:text-7xl font-bold text-[#8892b0]">
+            I'm a Front End Developer.
+          </h2>
+         
+          <div>
+            <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600">
+              View work
+              <span className="group-hover:rotate-90 duration-300">
+                <HiArrowNarrowRight className="ml-3" />
+              </span>
+            </button>
+          </div>
+        </div>
+      </motion.div>
+      <div className="avatar">
+        <motion.img 
+
+  layout
+  initial={{ opacity: 0.5,scale:0,x:500  }}
+  animate={{ opacity: 1, scale:1, x:0 }}
+  transition={{
+   
+    opacity: { ease: "linear" },
+    layout: { duration: 0.5 }
+  }}
+
+      src={Avatar}
+       className="md:h-[400px] md:w-[300px] object-cover rounded-full duration-300 w-[200px] h-[200px] hover:shadow-2xl hover:scale-[200%] hover:shadow-white "
+        alt="Thai Duy"
+      />
+
+      </div>
+      
+    </div>
+  );
+};
+
+export default Home;
--- .history/src/components/Navbar_20221024184729.tsx
+++ .history/src/components/Navbar_20221024184729.tsx
@@ -3,7 +3,7 @@ import { FaBars, FaTimes, FaGithub, FaFacebook } from "react-icons/fa";
 import { HiOutlineMail } from "react-icons/hi";
 import { BsFillPersonLinesFill, BsTelephoneFill } from "react-icons/bs";
 import { AiFillLinkedin } from "react-icons/ai";
-import {Link} from 'react-scroll'
+
 
 const Navbar = () => {
   const [isNavOpen, setIsNavOpen] = useState<Boolean>(false);
